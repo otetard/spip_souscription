749f77571872a680268a762e54d63495fcad7b3d
Thierry Eraud <t.eraud@ritimo.org>
2013-03-17 20:43:53 +0100

    Notification pour les utilisateurs. Les messages envoyés sont générés par des modèles pouvant être surchargés.

--- souscription_pipelines.php
+++ souscription_pipelines.php
@@ -27,4 +27,31 @@ function souscription_optimiser_base_disparus($flux){
   return $flux;
 }
 
+/**
+ * Envoi d'une notification après reglement
+ *
+ * @pipeline trig_bank_notifier_reglement
+ * @param  array $flux Données du pipeline
+ * @return array       Données du pipeline
+ */
+function souscription_trig_bank_notifier_reglement($flux) {
+    
+	$email = sql_getfetsel('courriel', 'spip_souscriptions', 'id_transaction='.intval($flux['args']['id_transaction']));
+	$sujet = '['.$GLOBALS['meta']['nom_site'].'] ';
+    if ($flux['args']['succes']) {
+		$sujet .= 'Confirmation de votre réglement';
+		$message = recuperer_fond('modeles/mail-souscription-succes', 
+									array('id_transaction' => $flux['args']['id_transaction']));
+    }
+    else {
+		$sujet .= 'Echec de votre réglement';
+		$message = recuperer_fond('modeles/mail-souscription-echec',
+									array('id_transaction' => $flux['args']['id_transaction']));
+    }
+	$envoyer_mail = charger_fonction('envoyer_mail','inc');
+	$envoyer_mail($email, $sujet, $message, $GLOBALS['meta']['email_webmaster']);
+    
+    return $flux;
+}
+
 ?>
