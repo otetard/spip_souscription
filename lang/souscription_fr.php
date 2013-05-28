999e54551ce2a7a81cb4c082f3f085c43f8abad7
Olivier Tétard <olivier.tetard@miskin.fr>
2013-05-28 19:50:18 +0200

    Mise à jour des fichiers de langue

--- lang/souscription_fr.php
+++ lang/souscription_fr.php
@@ -1,87 +1,102 @@
 <?php
-// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// Produit automatiquement par le plugin LangOnet a partir de la langue source fr
+// Module: souscription
+// Langue: fr
+// Date: 28-05-2013 19:48:54
+// Items: 72
 
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-	// S
-	'souscription_titre' => 'Souscription',
+// A
+	'ajouter_lien_souscription' => 'Ajouter cette souscription',
 
-	// C
+// C
 	'cfg_exemple' => 'Exemple',
 	'cfg_exemple_explication' => 'Explication de cet exemple',
 	'cfg_titre_parametrages' => 'Paramétrages',
+	'confirmer' => 'Valider',
+	'confirmer_creation_campagne' => 'Enregistrer',
 
-        // S
-        'titre_souscriptions' => 'Souscriptions',
+// E
+	'explication_montant_adhesion' => '<NEW>',
+	'explication_montant_don' => ' ',
+	'explication_montants' => 'Le montants divent être au format suivant: <code>[montant]|[description]</code> à raison d\'une entrée par ligne.',
+	'explication_objectif' => 'Permet d\'indiquer un objectif monétaire pour la campagne.<br/>Mettre 0 si la campagne n\'a pas d\'objectif.',
+	'explication_objectif_initial' => 'Permet d\'indiquer le niveau initial de la campagne. Ce champs peut être utilisé pour indiquer des dons ou adhésions n\'ayant pas été compatibilisées par le module de souscriptions.',
+	'explication_recu_fiscal' => ' ',
+	'explication_type_objectif' => 'Type de l\'objectif (influe le montant de l\'objectif)',
 
-	// T
-	'titre_configurer_souscriptions' => 'Souscriptions',
-	'titre_page_configurer_souscriptions' => 'Configuration des souscriptions',
-        'titre_page_souscriptions_tous' => 'Liste des souscriptions',
-        'titre_types_souscriptions' => 'Types de souscriptions',
-
-	// A
-	'ajouter_lien_souscription' => 'Ajouter cette souscription',
-
-	// C
-        'confirmer' => 'Valider',
-
-	// E
-	'explication_montants' => "Le montants divent être au format suivant: <code>[montant]|[description]</code> à raison d'une entrée par ligne.",
-	'explication_montant_adhesion' => "",
-	'explication_montant_don' => " ",
-	'explication_recu_fiscal' => " ",
-
-	// I
+// I
 	'icone_creer_souscription' => 'Créer une souscription',
+	'icone_creer_souscription_campagne' => 'Créer une campagne',
 	'icone_modifier_souscription' => 'Modifier cette souscription',
+	'icone_modifier_souscription_campagne' => 'Modifier cette campagne',
 	'info_1_souscription' => 'Une souscription',
+	'info_1_souscription_campagne' => 'Une campagne',
 	'info_aucun_souscription' => 'Aucune souscription',
+	'info_aucun_souscription_campagne' => 'Aucune campagne',
+	'info_nb_souscription_campagnes' => '@nb@ campagnes',
 	'info_nb_souscriptions' => '@nb@ souscriptions',
 	'info_souscriptions_auteur' => 'Les souscriptions de cet auteur',
 
-	// L
+// L
+	'label_activer_adhesion' => 'Activer le module d\'adhésions',
+	'label_activer_don' => 'Activer le module dons',
 	'label_adresse' => 'Adresse',
+	'label_campagne' => 'Campagne',
 	'label_code_postal' => 'Code postal',
 	'label_courriel' => 'Courriel',
-	'label_montant' => 'Montant',
-	'label_montant_liste' => 'Montant',
-	'label_envoyer_info' => "M'inscrire sur la liste d'information",
+	'label_description' => 'Description de la campagne',
+	'label_dispositions_fiscales_explications' => 'Texte de présentation des dispositions fiscales',
+	'label_envoyer_info' => 'M\'inscrire sur la liste d\'information',
+	'label_exporter_payes' => 'Exporter les souscriptions payés',
+	'label_exporter_tous' => 'Exporter toutes les souscriptions',
 	'label_id_transaction' => 'Identifiant de la transaction',
+	'label_informer_comite_local' => 'Me faire connaître auprès du comité local le plus proche',
+	'label_montant' => 'Montant',
+	'label_montant_actuel' => 'Niveau actuel de la campagne',
+	'label_montant_liste' => 'Montant',
+	'label_montants' => 'Montants',
+	'label_montants_explication' => 'Texte d\'explication des montants',
+	'label_montants_label' => 'Label pour le champ « montants »',
 	'label_nom' => 'Nom',
+	'label_objectif' => 'Niveau de l\'objectif',
+	'label_objectif_initial' => 'Niveau initial de la campagne',
+	'label_pays' => 'Pays',
 	'label_prenom' => 'Prénom',
 	'label_recu_fiscal' => 'Recevoir un reçu fiscal',
-	'label_ville' => 'Ville',
-	'label_pays' => 'Pays',
 	'label_telephone' => 'Téléphone',
-        'label_campagne' => 'Campagne',
-        'label_titre_campagne' => 'Titre de la campagne',
-        'label_exporter_payes' => 'Exporter les souscriptions payés',
-        'label_exporter_tous' => 'Exporter toutes les souscriptions',
-        'label_informer_comite_local' => "Me faire connaître auprès du comité local le plus proche",
-        'label_type_souscription' => "Type de souscription",
-        'label_activer_adhesion' => "Activer le module d'adhésions",
-        'label_activer_don' => "Activer le module dons",
-        'label_type_saisie' => "Types de saisies",
-        'label_montants' => "Montants",
+	'label_titre' => 'Titre',
+	'label_titre_campagne' => 'Titre de la campagne',
+	'label_type_objectif' => 'Type d\'objectif',
+	'label_type_saisie' => 'Types de saisies',
+	'label_type_souscription' => 'Type de souscription',
+	'label_ville' => 'Ville',
 
-	// T
+// S
+	'souscription_titre' => 'Souscription',
+
+// T
 	'texte_ajouter_souscription' => 'Ajouter une souscription',
 	'texte_changer_statut_souscription' => 'Cette souscription est :',
-	'titre_souscription' => 'Souscription',
-	'titre_souscriptions' => 'Souscriptions',
-        'titre_configuration_adhesions' => 'Adhésions',
-        'titre_configuration_dons' => 'Dons',
-
-        // V
-        'voir_exporter' => "Exporter les dons",
-
-	'label_montants_explication' => "Texte d'explication des montants",	
-	'label_montants_label' => "Label pour le champ «&nbsp;montants&nbsp;»",
-	'label_dispositions_fiscales_explications' => 'Texte de présentation des dispositions fiscales',
+	'titre_configuration_adhesions' => 'Adhésions',
+	'titre_configuration_dons' => 'Dons',
 	'titre_configuration_general' => 'Options générales',
-);
+	'titre_configurer_souscriptions' => 'Souscriptions',
+	'titre_logo_souscription_campagne' => 'Logo de cette campagne',
+	'titre_page_configurer_souscriptions' => 'Configuration des souscriptions',
+	'titre_page_souscriptions_tous' => 'Liste des souscriptions',
+	'titre_souscription' => 'Souscription',
+	'titre_souscription_campagne' => 'Campagne',
+	'titre_souscription_campagnes' => 'Campagnes',
+	'titre_souscriptions' => 'Souscriptions',
+	'titre_types_souscriptions' => 'Types de souscriptions',
 
+// V
+	'voir_exporter' => 'Exporter les dons',
+);
 ?>
