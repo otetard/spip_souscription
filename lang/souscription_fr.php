5624142859923cfef4340f7ed7e57e70ac7c7cd0
Cerdic <cedric@yterium.com>
2013-12-06 18:36:53 +0100

    chaine de langue manquante

--- lang/souscription_fr.php
+++ lang/souscription_fr.php
@@ -129,6 +129,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'message_nok_objectif_initial_valeur' => "Valeur de l'objectif initial invalide",
 	'message_nok_objectif_initial_supperieur_objectif' => "Valeur de l'objectif initial suppérieur à l'objectif",
 	'message_nok_objectif_invalide' => "Type d'objectif invalide",
+	'message_nok_montants' => 'Erreur dans le format',
 
 // S
 	'souscription_titre' => 'Souscription',
