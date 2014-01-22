95f955fa8c62a152f7daa67b37be81a549eeb5c9
Olivier Tétard <olivier.tetard@miskin.fr>
2014-01-22 15:49:19 +0100

    Traduction anglaise (de la partie publique essentiellement)

---
+++ lang/souscription_en.php
@@ -0,0 +1,63 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// Produit automatiquement par le plugin LangOnet a partir de la langue source fr
+// Module: souscription
+// Langue: en
+// Date: 22-01-2014 16:11:42
+// Items: 94
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// B
+	'bouton_exporter' => 'Export',
+
+// C
+	'confirmer' => 'Confirm',
+	'confirmer_creation_campagne' => 'Save',
+
+// E
+	'erreur_adhesion_champ_obligatoire' => 'Please fill out this field for memberships',
+	'erreur_champ_obligatoire' => 'Please fill out this filed',
+	'erreur_code_postal_invalide' => 'Invalid postal code',
+	'erreur_echec_creation_transaction' => 'Failed to create the transaction',
+	'erreur_formulaire' => 'Form contains errors',
+	'erreur_montant_invalide' => 'Invalid amount',
+	'erreur_montant_specifie_invalide' => 'The specified amount is invalid',
+	'erreur_pays_invalide' => 'Invalid country',
+	'erreur_recu_fiscal_champ_obligatoire' => 'Please fill out this field (because of tax receipt asked)',
+	'erreur_technique_formulaire' => 'Technical error: failed to prepare the transaction…',
+	'erreur_telephone_invalide' => 'Invalid phone number',
+	'explication_bloc_fiscal_adhesion' => 'The following information is necessary to became member:',
+	'explication_bloc_fiscal_don' => 'The following information is necessary so we can deliver you a tax receipt',
+
+// L
+	'label_adresse' => 'Adress',
+	'label_avancement_campagne_adhesions' => 'Campain progress: @nombre@ over @objectif@!',
+	'label_avancement_campagne_dons' => 'Campain progress: @montant@€ over @objectif@€!',
+	'label_code_postal' => 'Postal code',
+	'label_courriel' => 'E-mail',
+	'label_don_groupe' => 'Donation',
+	'label_envoyer_info' => 'Subscribe to the newsletter',
+	'label_general_groupe' => 'General options',
+	'label_legende_bloc_fiscal' => 'Additional information',
+	'label_montant' => 'Amount',
+	'label_montant_actuel' => 'Campain&#8217s actuel level',
+	'label_montant_libre' => 'Other amount:',
+	'label_montant_liste' => 'Amount',
+	'label_montants' => 'Amounts',
+	'label_nom' => 'Name',
+	'label_pays' => 'Country',
+	'label_prenom' => 'First name',
+	'label_recu_fiscal' => 'Receive a tax receipt',
+	'label_statut' => 'Status',
+	'label_telephone' => 'Phone',
+	'label_ville' => 'City',
+
+// S
+	'sous_titre_type_adhesions' => 'Membership campaign',
+	'sous_titre_type_dons' => 'Donation campaign',
+);
+?>
